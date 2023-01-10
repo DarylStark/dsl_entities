47eb7af63164b945d170bdd3cc354cb07f3b31a0
Daryl Stark <daryl@dstark.nl>
2023-01-10 21:30:29 +0100

    Added functions for the Entity Manager itself

--- include/dsl_entity_manager.h
+++ include/dsl_entity_manager.h
@@ -2,14 +2,22 @@
 #define __DSL_ENTITY_MANAGER_H__
 
 #include <unordered_map>
+#include <utility>
 #include "dsl_entity.h"
 
 namespace dsl
 {
     namespace entities
     {
+        typedef std::pair<bool, Entity &> register_return_type;
+
         // Export a global object for entities
         extern std::unordered_map<std::string, Entity> gem_n;
+
+        // Functions for the global entity manager
+        register_return_type register_entity(std::string name, Entity entity);
+        Entity &get_entity(std::string name, bool reg = true);
+        bool remove_entity(std::string name);
     }
 };
 
--- src/dsl_entity_manager.cpp
+++ src/dsl_entity_manager.cpp
@@ -5,6 +5,26 @@ namespace dsl
     namespace entities
     {
         // Global object to globablly manage NumericEntities
-        std::unordered_map<std::string, Entity> entities;
+        std::unordered_map<std::string, Entity> gem_n;
+
+        // Function to register a Entity to the global entity manager
+        register_return_type register_entity(std::string name, Entity entity)
+        {
+            const auto [it, success] = gem_n.insert({name, entity});
+            return {success, get_entity(name)};
+        }
+
+        Entity &get_entity(std::string name, bool reg /* = true */)
+        {
+            if (reg)
+                return gem_n[name];
+            return gem_n.at(name);
+        }
+
+        // Function to remove a Entity from the global entity manager
+        bool remove_entity(std::string name)
+        {
+            return gem_n.erase(name) > 0;
+        }
     };
 };
